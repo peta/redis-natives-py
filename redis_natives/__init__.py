6470cb9b771800f47a7bb86aed2517d4a5362c1f
peta <code@petergeil.name>
2010-07-12 00:03:18 +0200

    Fixed imports of module __init__

--- redis_natives/__init__.py
+++ redis_natives/__init__.py
@@ -1,13 +1,6 @@
 # legacy imports
-from redis_natives.datatypes import Primitive, Set, ZSet, Dict, List, Sequence
-from redis_natives.annotations import temporary, indexed, incremental, namespaced
-from redis_natives.natives import RedisNativeFactory
-
+from redis_natives import datatypes, natives, annotations, errors
 
 __version__ = '0.1'
 __author__ = 'Peter Geil'
-__all__ = (
-    'Primitive', 'Set', 'ZSet', 'Dict', 'List', 'Sequence',
-    'temporary', 'indexed', 'incremental', 'namespaced',
-    'RedisNativeFactory'
-)
+__all__ = ('datatypes', 'natives', 'annotations', 'errors')
