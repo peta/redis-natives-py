af3978a637a00573f0954df6c7ee47f9cefa5cdd
peta <code@petergeil.name>
2010-07-13 20:43:34 +0200

    Added metadata for PyPI support

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+"""
+@file setup.py
+@author Peter Geil
+@date 8/7/2010
+@brief Setuptools configuration for redis-natives-py
+"""
+
+version = '0.1'
+
+sdict = {
+    'name' : 'redis_natives',
+    'version' : version,
+    'description' : 'Exposes Redis entities as native Python datatypes. Simple, plain but powerful. Supports namespacing, indexing, and some more.',
+    'long_description' : 'A thin abstraction layer on top of redis-py that exposes Redis entities as native Python datatypes. Simple, plain but powerful. No ORMing or model-messing.',
+    'url': 'http://github.com/peta/redis-natives-py',
+    'download_url' : 'http://github.com/downloads/peta/redis-natives-py/redis-natives-py-%s.zip' % version,
+    'author' : 'Peter Geil',
+    'author_email' : 'code@petergeil.name',
+    'maintainer' : 'Peter Geil',
+    'maintainer_email' : 'code@petergeil.name',
+    'keywords' : ['Redis', 'key-value store', 'redis-py', 'datatypes', 'natives', 'helper'],
+    'license' : 'BSD',
+    'packages' : ['redis_natives'],
+	'requires': ['redis (>=2.0)'],
+    'test_suite' : 'tests.all_tests',
+    'classifiers' : [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+		'Topic :: Database'],
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+    
+setup(**sdict)
+
